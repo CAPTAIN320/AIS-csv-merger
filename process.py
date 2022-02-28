7c175f39d3aedfa9bb437ed3ccffad0d6fea2c3a
Yaqub Mahmoud <yaqub320@nowhere.co.jp>
2022-02-28 23:43:39 +0900

    Added comments

--- process.py
+++ process.py
@@ -7,20 +7,6 @@ import pandas as pd
 
 tsv_PATH = './tsv_from_AIS'
 
-csv_upload_dir = './csv_upload'
-
-
-
-tsv_files = glob.glob('./csv_from_AIS/*.tsv')
-frames = []
-for file in tsv_files:
-    lines = open(file, encoding="cp932").readlines()
-    open(file, 'w').writelines(lines[3:])
-    df = pd.read_csv(file, sep='\t', header=None)
-    frames.append(df)
-
-result = pd.concat(frames)
-
 columns = ['sku',
            'product-id',
            'product-id-type',
@@ -37,10 +23,29 @@ columns = ['sku',
            'item-note',
            'handling-time']
 
-result.columns = columns
+def merge_tsv_files(PATH):
+    # read tsv files
+    tsv_files = glob.glob(PATH+'/*.tsv')
+    # array of data frames
+    frames = []
+    for file in tsv_files:
+        # read lines in file
+        lines = open(file, encoding="cp932").readlines()
+        # remove the first 3 lines
+        open(file, 'w').writelines(lines[3:])
+        # convert file to dataframe
+        df = pd.read_csv(file, sep='\t', header=None)
+        # append data frame to array
+        frames.append(df)
 
-print(result)
+    # concantenate data frames
+    result = pd.concat(frames)
+    # add columns to data frame
+    result.columns = columns
+    # output data frame as tsv file
+    result.to_csv('./output.tsv', sep='\t', index=False)
+    # output data frame as excel file
+    # result.to_excel('./output.xlsx', index=False)
 
-result.to_csv('./output.tsv', sep='\t', index=False)
-# result.to_excel('./output.xlsx', index=False)
 
+merge_tsv_files(tsv_PATH)
