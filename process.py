2bd72b4f9dbad76dea3af4db98edda92ab0d5cb0
Yaqub Mahmoud <yaqub320@nowhere.co.jp>
2022-02-28 23:37:54 +0900

    Update process.py

--- process.py
+++ process.py
@@ -1,19 +1,19 @@
 import os
 import glob
-import shutil
 
 from pathlib import Path
 
 import pandas as pd
 
-csv_src_dir = './csv_from_AIS'
+tsv_PATH = './tsv_from_AIS'
 
 csv_upload_dir = './csv_upload'
 
-csv_files = glob.glob('./csv_from_AIS/*.tsv')
 
+
+tsv_files = glob.glob('./csv_from_AIS/*.tsv')
 frames = []
-for file in csv_files:
+for file in tsv_files:
     lines = open(file, encoding="cp932").readlines()
     open(file, 'w').writelines(lines[3:])
     df = pd.read_csv(file, sep='\t', header=None)
